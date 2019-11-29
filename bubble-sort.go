72b626e6d3b44ae713a97d825264b969784ac459
John McKee <jmckee3@mac.com>
2019-11-29 14:49:16 -0800

    working version

--- bubble-sort.go
+++ bubble-sort.go
@@ -1,3 +1,28 @@
 package main
 
+import "fmt"
+
+func countSwaps(a []int32) {
+	n := int32(len(a))
+	var temp int32
+	var swaps int32
+
+	for i := int32(0); i < n; i++ {
+
+		for j := int32(0); j < n-1; j++ {
+			// Swap adjacent elements if they are in decreasing order
+			if a[j] > a[j+1] {
+				temp = a[j]
+				a[j] = a[j+1]
+				a[j+1] = temp
+				swaps++
+			}
+		}
+	}
+
+	fmt.Printf("Array is sorted in %d swaps.\n", swaps)
+	fmt.Println("First Element:", a[0])
+	fmt.Println("Last Element:", a[n-1])
+}
+
 func main() {}
--- spec-bubble-sort_test.go
+++ spec-bubble-sort_test.go
@@ -2,6 +2,24 @@ package main
 
 import "testing"
 
-func TestBubbleSort(t *testing.T) {
+func TestBubbleSort1(t *testing.T) {
+	a := []int32{int32(6), int32(4), int32(1)}
 
+	countSwaps(a)
+
+	// expected output:
+	// Array is sorted in 3 swaps
+	// First Element: 1
+	// Last Element: 6
+}
+
+func TestBubbleSort2(t *testing.T) {
+	a := []int32{int32(1), int32(2), int32(3)}
+
+	countSwaps(a)
+
+	// expected output:
+	// Array is sorted in 0 swaps
+	// First Element: 1
+	// Last Element: 3
 }
